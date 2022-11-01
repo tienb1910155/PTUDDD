8ebbf36a9d8e0100b8fd05257498e164fb33d981
tienb1910155 <thanhtien123lx@gmail.com>
2022-11-01 12:06:30 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'ui/screen.dart';
 
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+  await dotenv.load(); //load .env file
   runApp(const MyApp());
 }
 
@@ -14,47 +16,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => ProductsManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => CartManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => OrdersManager(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'My Shop',
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.purple,
-          ).copyWith(
-            secondary: Colors.deepOrange,
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => AuthManager(),
           ),
-        ),
-        home: const ProductsOverviewScreen(),
-        routes: {
-          CartScreen.routeName: (ctx) => const CartScreen(),
-          OrdersScreen.routeName: (ctx) => const OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
-        },
-        onGenerateRoute: (settings){
-          if (settings.name == ProductDetailScreen.routeName){
-            final productId = settings.arguments as String;
-            return MaterialPageRoute(
-              builder: (ctx) {
-                return ProductDetailScreen(
-                  ctx.read<ProductsManager>().findById(productId),
-                );
+          ChangeNotifierProvider(
+            create: (ctx) => ProductsManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => CartManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => OrdersManager(),
+          ),
+        ],
+        child:Consumer<AuthManager>(
+          builder: (ctx, authManager, child){
+            return  MaterialApp(
+              title: 'My Shop',
+              theme: ThemeData(
+                fontFamily: 'Lato',
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.purple,
+                ).copyWith(
+                  secondary: Colors.deepOrange,
+                ),
+              ),
+              home: authManager.isAuth
+                  ? const ProductsOverviewScreen()
+                  : FutureBuilder(
+                future: authManager.tryAutoLogin(),
+                builder: (ctx, snapshot){
+                  return snapshot.connectionState == ConnectionState.waiting
+                      ? const SplashScreen()
+                      : const AuthScreen();
+                },
+              ),
+              routes: {
+                CartScreen.routeName: (ctx) => const CartScreen(),
+                OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+                UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+              },
+              onGenerateRoute: (settings){
+                if (settings.name == ProductDetailScreen.routeName){
+                  final productId = settings.arguments as String;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return ProductDetailScreen(
+                        ctx.read<ProductsManager>().findById(productId),
+                      );
+                    },
+                  );
+                }
+                if (settings.name == EditProductScreen.routeName){
+                  final productId = settings.arguments as String?;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return EditProductScreen(
+                        productId != null
+                            ? ctx.read<ProductsManager>().findById(productId)
+                            : null,
+                      );
+                    },
+                  );
+                }
+                return null;
               },
             );
-          }
-          return null;
-        },
-      ),
+          },
+        )
     );
   }
 }
--- lib/ui/shared/dialog_utils.dart
+++ lib/ui/shared/dialog_utils.dart
@@ -14,11 +14,29 @@ Future<bool?> showConfirmDialog(BuildContext context, String message){
           },
         ),
         TextButton(
-            onPressed: (){
-              Navigator.of(ctx).pop(true);
-            },
-            child: const Text('Yes'),
+          onPressed: (){
+            Navigator.of(ctx).pop(true);
+          },
+          child: const Text('Yes'),
         ),
+      ],
+    ),
+  );
+}
+
+Future<void> showErrorDialog(BuildContext context, String message) {
+  return showDialog(
+    context: context,
+    builder: (ctx) => AlertDialog(
+      title: const Text('An Error Occurred!'),
+      content: Text(message),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Okay'),
+          onPressed: () {
+            Navigator.of(ctx).pop();
+          },
+        )
       ],
     ),
   );
